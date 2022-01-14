10b4449d9b215770578f8e72e92b1775da6344e0
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2022-01-14 15:15:50 -0600

    all test passing but one

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -21,6 +21,13 @@ function validateProject(req, res, next) {
     next();
   }
 }
+function validateProjectStrict(req, res, next) {
+  if (!req.body.name || !req.body.description || !req.body.completed) {
+    next({ status: 400, message: "missing required fields" });
+  } else {
+    next();
+  }
+}
 
 function errorHandling(err, req, res, next) {
   res.status(err.status || 500).json({
