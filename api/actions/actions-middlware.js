f7077a85f683e6eeda079f5aa817a0f2851cfacb
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2022-01-14 13:43:53 -0600

    worked on the return of array of actions. MVP not complete yet'

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,35 @@
-// add middlewares here related to actions
+const Action = require("./actions-model");
+
+async function validateActionId(req, res, next) {
+  try {
+    const action = await Action.get(req.params.id);
+    if (action) {
+      req.action = action;
+      next();
+    } else {
+      next({ status: 404, message: "Action not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function validateAction(req, res, next) {
+  if (!req.body.project_id || !req.body.description || !req.body.notes) {
+    next({ status: 400, message: "Missing required fields" });
+  } else {
+    next();
+  }
+}
+
+function errorHandling(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: `${err.message}`,
+  });
+}
+
+module.exports = {
+  validateActionId,
+  validateAction,
+  errorHandling,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,17 +2,62 @@ const express = require("express");
 const Action = require("./actions-model");
 const router = express.Router();
 
+const {
+  validateActionId,
+  validateAction,
+  errorHandling,
+} = require("./actions-middlware");
+
 router.get("/", (req, res) => {
   Action.get()
     .then((actions) => {
       res.status(200).json(actions);
     })
     .catch((error) => {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: "The posts information could not be retrieved" });
+      next(error);
     });
 });
+
+router.get("/:id", validateActionId, (req, res) => {
+  Action.get(req.params.id)
+    .then((action) => {
+      res.json(action);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.post("/", validateAction, (req, res, next) => {
+  Action.insert(req.body)
+    .then((newAction) => {
+      res.status(200).json(newAction);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.put("/:id", validateAction, validateActionId, (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then((updatedAction) => {
+      res.status(200).json(updatedAction);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.delete("/:id", validateActionId, (req, res) => {
+  Action.remove(req.params.id)
+    .then((deletedNumber) => {
+      res.status(200).json(deletedNumber);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.use(errorHandling);
 
 module.exports = router;
